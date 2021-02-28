c5bd6eb5e5cabd96a1fc31e187a8fe4d38216ba6
chisom <iwowochisom@gmail.com>
2021-02-28 19:43:05 +0100

    modify cloud gateway application

--- src/main/java/com/chisom/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/com/chisom/cloudgateway/CloudGatewayApplication.java
@@ -2,6 +2,7 @@ package com.chisom.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,6 +18,7 @@ public class CloudGatewayApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
